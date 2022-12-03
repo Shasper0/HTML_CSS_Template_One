d06431aa76efce3b1406887c7769dc9ae7e5740a
Shasper0 <109701515+Shasper0@users.noreply.github.com>
2022-12-03 14:55:49 +0200

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let links = document.getElementsByClassName('links')[0];
-let ul = document.getElementsByTagName('ul')[0];
-
-
-links.addEventListener("click" , function(){
-    ul.classList.toggle('i');
-} )
